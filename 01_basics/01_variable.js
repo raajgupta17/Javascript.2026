aa1a682a164b8a9faabf2cca1d23fb753891588f
Raaj Gupta <guptaraaj4464@gmail.com>
2026-04-05 03:48:42 +0000

    contanier config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=8101112328
+let accountHolderName= "Raaj"
+var accountEmail="guptaraaj@1122.c"
+accountCity="Siliguri"
+let accountState;
+
+// accountId=9614407 constant cant be change
+
+accountHolderName="Aman"
+accountEmail="guptaraaj@2266.c"
+accountCity="Bagdogra"
+
+/* Prefer not to use var because of issue in block scope and functional scope
+instead use let */
+
+
+
+console.log(accountId);
+console.table([accountHolderName,accountEmail,accountCity,accountState])
