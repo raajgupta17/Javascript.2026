a68b89124a0692f848c0e8d0cf7e34f09aba3783
Raaj Gupta <guptaraaj4464@gmail.com>
2026-04-05 04:15:24 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,24 @@
+"Use Strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not brower so it will show error
+
+//code readability should be high use of comment , proper spacing should be good 
+
+let name= "Aman"
+let age= 20
+let isLoggedIn=true 
+
+//number => range 2 to power 53
+//bigInt => very large number
+//string = ""
+//boolean => true/false
+//nul => standalone value (does not mean its 0)
+//undefined 
+//symbol => mainly use for symbol  
+
+
+// object 
+
+
+console.log(typeof null ); // type of null is obejct 
+console.log(typeof undefined ); // type of undefined will be undefined
