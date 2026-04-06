c7c9f346c959ef33b2e2b1a02f1c93fcfcf3eae8
Raaj Gupta <guptaraaj4464@gmail.com>
2026-04-06 17:35:54 +0000

    Operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,38 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+ 
+
+let str1= "hello"
+let str2=" Aman"
+
+let str3 = str1+str2
+// console.log(str3);
+
+
+// console.log(typeof (1 + "2")); //  it will output 12 as string
+// console.log("1" + 2+2); // it will give output 122 because pehla wo check karaga usko string dikhega so totaly to string bana lega
+// console.log(1 + 2+"2"); // iska output 32 hoga
+// console.log(1 + "2"+2); //iska bhi output 122 hoga 
+
+
+let gameCounter = 100
+gameCounter++;   //++gameCounter; -> prefix.       gameCounter++ -> postfix 
+console.log(gameCounter);
+
+
+ 
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
